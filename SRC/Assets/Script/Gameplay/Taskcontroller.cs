3fe1b563a6f7c325a69fa65aa1fd927207fcdcfe
ApisSud <apisaragrs@gmail.com>
2026-02-06 00:01:57 +0700

    ImportSFX

--- SRC/Assets/Script/Gameplay/Taskcontroller.cs
+++ SRC/Assets/Script/Gameplay/Taskcontroller.cs
@@ -6,21 +6,33 @@ public class Taskcontroller : MonoBehaviour
 {
     public Image[] Taskimage;
     public GameObject[] pages;
-    
-    
- 
+    [SerializeField] AudioSource Clickingmouse;
+
     void Start()
     {
-        ActivateTask(0);
+       
+        UpdatePageVisuals(0);
     }
 
-  public void ActivateTask(int taskNo)
+    public void ActivateTask(int taskNo)
     {
-        for (int i = 0 ; i < pages.Length; i++)
+        if (Clickingmouse != null)
+        {
+            Clickingmouse.Play();
+        }
+
+        UpdatePageVisuals(taskNo);
+    }
+
+ 
+    private void UpdatePageVisuals(int taskNo)
+    {
+        for (int i = 0; i < pages.Length; i++)
         {
             pages[i].SetActive(false);
             Taskimage[i].color = Color.grey;
         }
+
         pages[taskNo].SetActive(true);
         Taskimage[taskNo].color = Color.white;
     }

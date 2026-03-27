aea5fce7de5f3ba1a17c91c5e3dd1827219282f8
Natto57 <pat.natthanicha@gmail.com>
2026-03-28 04:37:08 +0700

    AreaFloor2

--- SRC/Assets/Script/GameManager.cs
+++ SRC/Assets/Script/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     [SerializeField]
     private Level level;
-
+    private bool floor2;
     [SerializeField] int minX = -6, maxX = 2;
     [SerializeField] int minY = -6, maxY = 2;
     void Awake()
@@ -26,9 +26,16 @@ public class GameManager : MonoBehaviour
 
     public bool IsWithinBounds(Vector3Int gridPosition)
     {
-      
-        return (gridPosition.x >= minX && gridPosition.x <= maxX &&
-                gridPosition.y >= minY && gridPosition.y <= maxY);
+        if(level == Level.fairy)
+        {
+                floor2 = (gridPosition.x >= 0 && gridPosition.x <= 15 &&
+                gridPosition.y >= 8 && gridPosition.y <= 13) | (gridPosition.x >= 10 && gridPosition.x <= 15 &&
+                gridPosition.y >= -4 && gridPosition.y <= 7);
+        }
+
+        bool mainArea = gridPosition.x >= minX && gridPosition.x <= maxX && gridPosition.y >= minY && gridPosition.y <= maxY;
+
+        return mainArea | floor2;
 
 
     }

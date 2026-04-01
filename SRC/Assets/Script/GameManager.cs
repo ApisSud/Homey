cefa60ba0aa916ed82cd92fa98bb9e2dd478bf72
Natto57 <pat.natthanicha@gmail.com>
2026-04-01 17:39:05 +0700

    AreaWitchHouse

--- SRC/Assets/Script/GameManager.cs
+++ SRC/Assets/Script/GameManager.cs
@@ -32,8 +32,15 @@ public class GameManager : MonoBehaviour
                 gridPosition.y >= 8 && gridPosition.y <= 13) | (gridPosition.x >= 10 && gridPosition.x <= 15 &&
                 gridPosition.y >= -4 && gridPosition.y <= 7);
         }
+        else if(level == Level.witch)
+        {
+            floor2 = (gridPosition.x >= 2 && gridPosition.x <= 5 &&
+               gridPosition.y >= -10 && gridPosition.y <= -7) | (gridPosition.x >= 4 && gridPosition.x <= 7 &&
+               gridPosition.y >= -2 && gridPosition.y <= 1) | (gridPosition.x >= -2 && gridPosition.x <= 7 &&
+               gridPosition.y >= 2 && gridPosition.y <= 5) ;
+        }
 
-        bool mainArea = gridPosition.x >= minX && gridPosition.x <= maxX && gridPosition.y >= minY && gridPosition.y <= maxY;
+            bool mainArea = gridPosition.x >= minX && gridPosition.x <= maxX && gridPosition.y >= minY && gridPosition.y <= maxY;
 
         return mainArea | floor2;
 

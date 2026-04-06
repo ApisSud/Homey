d692e2c18769a926d1df795f6bb0760ac981f953
ApisSud <apisaragrs@gmail.com>
2026-04-06 18:46:26 +0700

    ANIMATION

--- SRC/Assets/Script/Gameplay/Furnitureinteract.cs
+++ SRC/Assets/Script/Gameplay/Furnitureinteract.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
 
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Animator))]
+
 public class Furnitureinteract : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator animator;
+
+   
+
     void Start()
     {
-        
+      
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetMouseButtonDown(1))
+        {
+           
+            Debug.Log("rmb!");
+
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+            
+            if (hit.collider != null && hit.collider.gameObject == this.gameObject)
+            {
+                Debug.Log("play anim");
+                animator.SetTrigger("Playanim");
+            }
+        }
     }
 }
